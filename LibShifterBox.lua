c3f56099dd060e7605cb11d069b4fbf3eaea139c
Fabian Schär <klingo@klingonium.com>
2019-08-03 12:03:36 +0200

    initialised some files

---
+++ LibShifterBox.lua
@@ -0,0 +1,20 @@
+local LIB_IDENTIFIER = "LibShifterBox"
+assert(not _G[LIB_IDENTIFIER], LIB_IDENTIFIER .. " is already loaded")
+
+local LibShifterBox = {}
+_G[LIB_IDENTIFIER] = LibShifterBox
+
+-- constants
+
+
+
+-- private functions
+local function myInternalFunction()
+
+end
+
+
+-- public functions
+function LibShifterBox:PublicFunction()
+
+end
